67b8ea55e0050a7789b694835d3e6cce3d1ef6f3
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-14 18:37:29 +0700

    Init commit for dev branch

---
+++ settings.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "aq-tf-cloud"
+
+    workspaces {
+      name = "AZ-ACLOUDGURU"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "AWS Region"
+  # Default region for acloudguru is us-east-1
+  default = "us-east-1"
+}
