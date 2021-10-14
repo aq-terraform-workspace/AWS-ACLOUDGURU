1f9658a621159ac2d4cb10a84e67928754ebd16a
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-14 20:05:35 +0700

    Test cloudflare

---
+++ route53.tf
@@ -0,0 +1,18 @@
+# Create AWS Route53 and point the Cloudflare domain to these name servers
+resource "random_integer" "random" {
+  min = 1
+  max = 99999999
+}
+module "route53" {
+  source      = "git::https://github.com/aq-terraform-modules/terraform-aws-route53.git?ref=dev"
+  domain_name = "${local.sub_domain_prefix}-${random_integer.random.result}.${local.main_domain}"
+}
+output "name_servers" {
+  value = module.route53.route53_name_servers
+}
+data "cloudflare_zone" "example" {
+  name = "pierre-cardin.info"
+}
+output "cloudflare_zone" {
+  value = data.cloudflare_zone.example.status
+}
