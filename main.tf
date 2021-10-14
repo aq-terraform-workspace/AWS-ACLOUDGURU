2088fb21960c8a883abb65b89ba14dcd3ad754b9
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-14 19:48:20 +0700

    Update route53 to a random uuid

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
 # Create AWS Route53 and point the Cloudflare domain to these name servers
+locals {
+  main_domain = "pierre-cardin.info"
+  sub_domain_prefix = "aws"
+}
+
+resource "random_integer" "random" {
+  min = 1
+  max = 99999999
+}
 module "route53" {
   source      = "git::https://github.com/aq-terraform-modules/terraform-aws-route53.git?ref=dev"
-  domain_name = "aws-${uuid()}.pierre-cardin.info"
+  domain_name = "${local.sub_domain_prefix}-${random_integer.random.result}.${local.main_domain}"
 }
 output "name_servers" {
   value = module.route53.route53_name_servers
