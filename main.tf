0019919b9681c0bd986d28013d07d08340f68061
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 19:18:21 +0700

    Add name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "random_integer" "random" {
 
 module "base_network" {
   source      = "git::https://github.com/aq-terraform-modules/terraform-aws-base-network.git?ref=dev"
-
+  name       = "core"
   cidr_block = "139.0.0.0/16"
   azs        = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets  = ["139.0.10.0/24", "139.0.11.0/24", "139.0.12.0/24"]
