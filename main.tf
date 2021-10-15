80339236a004e5bf9ebf638be7058ce5dbc4bc4a
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 17:42:53 +0700

    Update cloudflare module

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "base_network" {
   source      = "git::https://github.com/aq-terraform-modules/terraform-aws-base-network.git?ref=dev"
 
   cidr_block = "139.0.0.0/16"
-  azs        = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+  azs        = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets  = ["139.0.10.0/24", "139.0.11.0/24", "139.0.12.0/24"]
   private_subnets = ["139.0.20.0/24", "139.0.21.0/24", "139.0.22.0/24"]
 }
--- route53.tf
+++ route53.tf
@@ -1,11 +1,19 @@
 # Create AWS Route53 and point the Cloudflare domain to these name servers
 module "route53" {
-  providers = {
-    aws        = aws
-    cloudflare = cloudflare
-  }
-
   source      = "git::https://github.com/aq-terraform-modules/terraform-aws-route53.git?ref=dev"
   main_domain = local.main_domain
   sub_domain  = "${local.sub_domain}-${random_integer.random.result}"
+}
+
+module "cloudflare_records" {
+  providers = {
+    cloudflare = cloudflare
+  }
+
+  source       = "git::https://github.com/aq-terraform-modules/terraform-cloudflare-general.git?ref=dev"
+  main_domain  = local.main_domain
+  sub_domain   = "${local.sub_domain}-${random_integer.random.result}"
+  name_servers = module.route53.name_servers
+
+  depends_on = [module.route53]
 }
