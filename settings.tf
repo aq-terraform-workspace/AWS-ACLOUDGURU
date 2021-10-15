41a17cd4df744bd9569108fcca85f99932a561aa
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 14:49:21 +0700

    Update provider setting

--- settings.tf
+++ settings.tf
@@ -14,7 +14,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
-  acces_key = var.aws_access_key
+  access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
