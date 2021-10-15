6b6cd4438a20b6651b3ee5fb284dd44400e58100
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 19:02:36 +0700

    Remmove alb

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
-module "alb" {
+/* module "alb" {
   source      = "git::https://github.com/aq-terraform-modules/terraform-aws-alb.git?ref=dev"
   name        = "core-${random_integer.random.result}"
   subnets     = module.base_network.public_subnets
-}
+} */
