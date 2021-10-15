cf59765831fac5500dfa2ed9c5dcaeb82ecf75a9
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 17:19:17 +0700

    Add base-network

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ resource "random_integer" "random" {
   min = 1
   max = 99999999
 }
+
+module "base_network" {
+  source      = "git::https://github.com/aq-terraform-modules/terraform-aws-base-network.git?ref=dev"
+
+  cidr_block = "139.0.0.0/16"
+  azs        = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+  public_subnets  = ["139.0.10.0/24", "139.0.11.0/24", "139.0.12.0/24"]
+  private_subnets = ["139.0.20.0/24", "139.0.21.0/24", "139.0.22.0/24"]
+}
