b433e88abcf8889d4b5fbb0bc61a8417e724e110
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-20 03:23:38 +0700

    Not manage the aws_auth

--- eks.tf
+++ eks.tf
@@ -32,20 +32,6 @@ module "eks" {
   manage_aws_auth = false
 }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
 /* data "aws_lb_target_group" "target_group" {
   name = var.alb_target_group_name
 }
