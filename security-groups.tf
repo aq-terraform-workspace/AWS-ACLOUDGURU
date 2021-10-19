35dd7bb77754fbe27b966a29ba068af612102891
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-19 21:08:30 +0700

    Change name

--- security-groups.tf
+++ security-groups.tf
@@ -1,4 +1,4 @@
-module "dmz_sg" {
+module "sg_dmz" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.4.0"
 
@@ -32,11 +32,11 @@ module "dmz_sg" {
   ]
 }
 
-module "dmz_alb" {
+module "sg_alb" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.4.0"
 
-  name    = "alb_dmz"
+  name    = "sg_alb"
   description = "Security group for Load Balancer"
   vpc_id      = module.base_network.vpc_id
 
@@ -58,11 +58,11 @@ module "dmz_alb" {
   ]
 }
 
-module "eks_alb" {
+module "sg_eks" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.4.0"
 
-  name    = "eks_dmz"
+  name    = "sg_eks"
   description = "Security group for EKS"
   vpc_id      = module.base_network.vpc_id
 
@@ -72,28 +72,28 @@ module "eks_alb" {
       to_port     = 80
       protocol    = "tcp"
       description = "Allow HTTP from LB"
-      source_security_group_id = module.dmz_alb.security_group_id
+      source_security_group_id = module.sg_alb.security_group_id
     },
     {
       from_port   = 443
       to_port     = 443
       protocol    = "tcp"
       description = "Allow HTTPS from LB"
-      source_security_group_id = module.dmz_alb.security_group_id
+      source_security_group_id = module.sg_alb.security_group_id
     },
     {
       from_port   = 5432
       to_port     = 5432
       protocol    = "tcp"
       description = "Allow Postgres"
-      source_security_group_id = module.postgres_sg.security_group_id
+      source_security_group_id = module.sg_postgres.security_group_id
     },
     {
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
       description = "Allow all from Bastion"
-      source_security_group_id = module.dmz_sg.security_group_id
+      source_security_group_id = module.sg_dmz.security_group_id
     }
   ]
 }
@@ -112,14 +112,14 @@ module "postgres_sg" {
       to_port     = 5432
       protocol    = "tcp"
       description = "Allow Postgres from Bastion"
-      source_security_group_id = module.dmz_sg.security_group_id
+      source_security_group_id = module.sg_dmz.security_group_id
     },
     {
       from_port   = 5432
       to_port     = 5432
       protocol    = "tcp"
       description = "Allow Postgres from EKS"
-      source_security_group_id = module.eks_alb.security_group_id
+      source_security_group_id = module.sg_eks.security_group_id
     }
   ]
 }
