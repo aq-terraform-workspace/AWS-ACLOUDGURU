9d06340ffa3e5c96740fa018bc30c7fc8f971bca
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-20 00:38:04 +0700

    Edit eks

--- eks.tf
+++ eks.tf
@@ -37,3 +37,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
