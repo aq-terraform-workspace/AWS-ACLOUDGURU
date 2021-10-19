5d1cab1d3071091ea67ce3776911896ecdf545e6
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-19 19:53:54 +0700

    Create DB

---
+++ rds.tf
@@ -0,0 +1,29 @@
+module "postgres" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "3.4.0"
+
+  identifier = var.db_name
+
+  engine            = "postgres"
+  engine_version    = "13.4"
+  instance_class    = var.instance_class
+  allocated_storage = var.allocated_storage
+
+  # DB subnet group
+  create_db_subnet_group = false
+  db_subnet_group_name = module.base_network.database_subnet_group_name
+
+  # Upgrade control
+  auto_minor_version_upgrade = true
+  allow_major_version_upgrade = false
+
+  # Configuration
+  multi_az = true
+  name     = var.db_name
+  username = var.db_username
+  create_random_password = true
+  random_password_length = 10
+  port     = "5432"
+  publicly_accessible = false
+
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,11 @@ variable "region" {
   # Default region for acloudguru is us-east-1
   default = "us-east-1"
 }
+
+variable "instance_class" {
+  description = "Instance class for RDS"
+}
+
+variable "allocated_storage" {
+  description = "RDS allocated storage"
+}
