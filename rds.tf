1b118e13dd581fa773fb961faf399b84e12c6bce
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-19 21:17:50 +0700

    Add DB

--- rds.tf
+++ rds.tf
@@ -1,13 +1,24 @@
-/* module "postgres" {
+module "postgres" {
   source  = "terraform-aws-modules/rds/aws"
   version = "3.4.0"
 
   identifier = var.db_name
 
+  # DB instance configuration
   engine            = "postgres"
   engine_version    = "13.4"
   instance_class    = var.instance_class
   allocated_storage = var.allocated_storage
+  vpc_security_group_ids = module.sg_database.security_group_id
+  publicly_accessible = false
+  storage_encrypted = false
+  storage_type = var.db_storage_type
+  multi_az = true
+  name     = var.db_name
+  username = var.db_username
+  create_random_password = true
+  random_password_length = 10
+  port     = "5432"
 
   # DB subnet group
   create_db_subnet_group = false
@@ -20,15 +31,4 @@
   # Parameter group and option group
   create_db_option_group = false
   create_db_parameter_group = false
-
-  # Configuration
-  multi_az = true
-  name     = var.db_name
-  username = var.db_username
-  create_random_password = true
-  random_password_length = 10
-  port     = "5432"
-  publicly_accessible = false
-  storage_encrypted = false
-  storage_type = var.db_storage_type
-} */
+}
