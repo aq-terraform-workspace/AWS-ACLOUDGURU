daba61a76ce5d39a0fefbe70b8c775e52fc24b05
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-20 15:14:26 +0700

    Create API

--- api-gw.tf
+++ api-gw.tf
@@ -13,6 +13,10 @@ module "apigateway-v2" {
   name = module.apigw_label.id
   description = "API Gateway for ${module.apigw_label.stage}"
   protocol_type = "HTTP"
-
+  create_api_domain_name = false
+  create_routes_and_integrations = false
+  create_vpc_link = false
   target = "http://${module.alb.lb_dns_name}"
+
+  tags = module.apigw_label.tags
 }
