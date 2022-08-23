9138cbadb0210261ed07abb90e1339fa1fdc0278
Anh Quach <61163704+anhqqt@users.noreply.github.com>
2022-08-23 17:49:58 +0700

    Add kms for crossaccount

--- kms-demo.tf
+++ kms-demo.tf
@@ -2,3 +2,9 @@ resource "aws_kms_key" "backup-key" {
   description             = "KSM Key for Backup Vault"
   deletion_window_in_days = 10
 }
+
+resource "aws_kms_key" "crossaccount-backup-key" {
+  provider                = aws.crossaccount
+  description             = "KSM Key for Backup Vault"
+  deletion_window_in_days = 10
+}
