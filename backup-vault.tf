8de9828d050a5d38374442807689aa37f392858e
Anh Quach <61163704+anhqqt@users.noreply.github.com>
2022-08-24 13:28:50 +0700

    Update

--- backup-vault.tf
+++ backup-vault.tf
@@ -76,7 +76,7 @@ resource "aws_backup_plan" "backup-plan" {
     rule {
         rule_name = "some-backup-plan-rule-name-3"
         target_vault_name = aws_backup_vault.backup-vault.name
-        schedule = "cron(0 0 * * * *)"     #adjust the time 
+        schedule = "cron(0 * * * ? *)"     #adjust the time 
         recovery_point_tags = {
             Type = "my-test-backup"
         }
