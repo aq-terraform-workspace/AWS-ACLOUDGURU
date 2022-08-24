0156e399d5aaa794a5430b366b8025817942720d
Anh Quach <61163704+anhqqt@users.noreply.github.com>
2022-08-24 12:19:48 +0700

    Test

--- backup-vault.tf
+++ backup-vault.tf
@@ -58,6 +58,20 @@ resource "aws_backup_plan" "backup-plan" {
             destination_vault_arn = aws_backup_vault.diff-account-vault.arn 
         }
     }
+
+    rule {
+        rule_name = "some-backup-plan-rule-name-2"
+        target_vault_name = aws_backup_vault.backup-vault.name
+        schedule = "cron(15 * ? * * *)"     #adjust the time 
+        start_window = 60
+        completion_window = 600
+        recovery_point_tags = {
+            Type = "my-test-backup"
+        }
+        copy_action {
+            destination_vault_arn = aws_backup_vault.diff-account-vault.arn 
+        }
+    }
 }
 
 # AWS Backup selection with tags
