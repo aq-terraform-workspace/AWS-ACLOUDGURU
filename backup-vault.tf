85c5cc61b6844a7c9f0edbc333c43e8e93a02748
Anh Quach <61163704+anhqqt@users.noreply.github.com>
2022-08-24 10:25:15 +0700

    Add backup global setting and regional setting

--- backup-vault.tf
+++ backup-vault.tf
@@ -1,3 +1,31 @@
+# Setting for AWS Backup
+resource "aws_backup_global_settings" "test" {
+  global_settings = {
+    "isCrossAccountBackupEnabled" = "true"
+  }
+}
+
+resource "aws_backup_region_settings" "test" {
+  resource_type_opt_in_preference = {
+    "Aurora"          = true
+    "DocumentDB"      = true
+    "DynamoDB"        = true
+    "EBS"             = true
+    "EC2"             = true
+    "EFS"             = true
+    "FSx"             = true
+    "Neptune"         = true
+    "RDS"             = true
+    "Storage Gateway" = true
+    "VirtualMachine"  = true
+  }
+
+  resource_type_management_preference = {
+    "DynamoDB" = true
+    "EFS"      = true
+  }
+}
+
 # AWS Backup vault
 resource "aws_backup_vault" "backup-vault" {
     name = "some-backup-vault-name"
