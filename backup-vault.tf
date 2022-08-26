e38359968ee30ca3be63f51467c101482b93c2aa
Anh Quach <61163704+anhqqt@users.noreply.github.com>
2022-08-26 11:40:26 +0700

    Update

--- backup-vault.tf
+++ backup-vault.tf
@@ -1,103 +1,111 @@
 # Setting for source AWS Backup
 resource "aws_backup_global_settings" "source" {
-   global_settings = {
-      "isCrossAccountBackupEnabled" = "true"
-   }
+	global_settings = {
+		"isCrossAccountBackupEnabled" = "true"
+	}
 }
 
 resource "aws_backup_region_settings" "source" {
-  resource_type_opt_in_preference = {
-      "Aurora"          = true
-      "DocumentDB"      = true
-      "DynamoDB"        = true
-      "EBS"             = true
-      "EC2"             = true
-      "EFS"             = true
-      "FSx"             = true
-      "Neptune"         = true
-      "RDS"             = true
-      "Storage Gateway" = true
-      "VirtualMachine"  = true
-  }
+	resource_type_opt_in_preference = {
+		"Aurora"          = true
+		"DocumentDB"      = true
+		"DynamoDB"        = true
+		"EBS"             = true
+		"EC2"             = true
+		"EFS"             = true
+		"FSx"             = true
+		"Neptune"         = true
+		"RDS"             = true
+		"Storage Gateway" = true
+		"VirtualMachine"  = true
+	}
 
-  resource_type_management_preference = {
-      "DynamoDB" = true
-      "EFS"      = true
-  }
+	resource_type_management_preference = {
+		"DynamoDB" = true
+		"EFS"      = true
+	}
 }
 
 # Setting for destination AWS Backup
 resource "aws_backup_global_settings" "dest" {
-   provider = aws.crossbackup
-   global_settings = {
-      "isCrossAccountBackupEnabled" = "true"
-   }
+	provider = aws.crossbackup
+	global_settings = {
+		"isCrossAccountBackupEnabled" = "true"
+	}
 }
 
 resource "aws_backup_region_settings" "dest" {
-   provider = aws.crossbackup
-   resource_type_opt_in_preference = {
-      "Aurora"          = true
-      "DocumentDB"      = true
-      "DynamoDB"        = true
-      "EBS"             = true
-      "EC2"             = true
-      "EFS"             = true
-      "FSx"             = true
-      "Neptune"         = true
-      "RDS"             = true
-      "Storage Gateway" = true
-      "VirtualMachine"  = true
-   }
+	provider = aws.crossbackup
+	resource_type_opt_in_preference = {
+		"Aurora"          = true
+		"DocumentDB"      = true
+		"DynamoDB"        = true
+		"EBS"             = true
+		"EC2"             = true
+		"EFS"             = true
+		"FSx"             = true
+		"Neptune"         = true
+		"RDS"             = true
+		"Storage Gateway" = true
+		"VirtualMachine"  = true
+	}
 
-   resource_type_management_preference = {
-      "DynamoDB" = true
-      "EFS"      = true
-   }
+	resource_type_management_preference = {
+		"DynamoDB" = true
+		"EFS"      = true
+	}
 }
 
 # AWS Backup vault
 resource "aws_backup_vault" "backup-vault" {
-   name = "some-backup-vault-name"
-   kms_key_arn = aws_kms_key.backup-key.arn
-   tags = {
-      Type = "my-test-backup"
-   }
+	name = "some-backup-vault-name"
+	kms_key_arn = aws_kms_key.backup-key.arn
+	tags = {
+		Type = "my-test-backup"
+	}
+	depends_on = [
+		aws_backup_global_settings.source,
+		aws_backup_region_settings.source
+	]
 }
 
 # Cross Account backup vault
 resource "aws_backup_vault" "diff-account-vault" {
-   provider = aws.crossbackup
-   name = "some-cross-account-vault-name"
-   kms_key_arn = aws_kms_key.crossbackup-backup-key.arn
+	provider = aws.crossbackup
+	name = "some-cross-account-vault-name"
+	kms_key_arn = aws_kms_key.crossbackup-backup-key.arn
+	depends_on = [
+		aws_backup_global_settings.dst,
+		aws_backup_region_settings.dst
+	]
 }
 
 # AWS Backup plan
 resource "aws_backup_plan" "backup-plan" {
-   name = "some-backup-plan-name"
-   rule {
-      rule_name = "some-backup-plan-rule-name"
-      target_vault_name = aws_backup_vault.backup-vault.name
-      schedule = "cron(30 * ? * * *)"     #adjust the time 
-      recovery_point_tags = {
-         Type = "my-test-backup"
-      }
-      copy_action {
-         destination_vault_arn = aws_backup_vault.diff-account-vault.arn 
-      }
-   }
+	name = "some-backup-plan-name"
+	rule {
+		rule_name = "some-backup-plan-rule-name"
+		target_vault_name = aws_backup_vault.backup-vault.name
+		schedule = "cron(30 * ? * * *)"     #adjust the time 
+		recovery_point_tags = {
+			Type = "my-test-backup"
+		}
+		copy_action {
+			destination_vault_arn = aws_backup_vault.diff-account-vault.arn 
+		}
+	}
 }
 
 # AWS Backup selection with tags
 resource "aws_backup_selection" "backup-selection" {
-   name = "some-backup-selection-name"
-   iam_role_arn = aws_iam_role.aws-backup-service-role.arn
-   plan_id = aws_backup_plan.backup-plan.id
-   selection_tag {
-      type = var.selection-type
-      key = "Type"
-      value = "my-test-backup"
-   }
+	name = "some-backup-selection-name"
+	iam_role_arn = aws_iam_role.aws-backup-service-role.arn
+	plan_id = aws_backup_plan.backup-plan.id
+	selection_tag {
+		type = var.selection-type
+		key = "Type"
+		value = "my-test-backup"
+	}
 }
 
 resource "aws_backup_vault_policy" "organization-level-policy" {
